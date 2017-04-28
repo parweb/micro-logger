212cb77d76a94ef273bf308a2a9f96ceb15629f0
parweb <parweb@gmail.com>
2017-04-28 14:27:13 +0200

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+const MongoDB = require('winston-mongodb').MongoDB;
+
+const options = {
+    db : 'mongodb://host:port/dbname',
+    username : 'user',
+    password : 'password',
+    collection : 'logs',
+    level : 'info',
+    storeHost: true
+};
+
+winston.loggers.add('mongoLog',{
+    transports : [
+        new(winston.transports.MongoDB)(options),
+    ]
+});
+
+var logger = winston.loggers.get('mongoLog')
+
+module.exports = async (req, res) => {
+	logger.info({'test message': 'plop'}, { 'plop' : 'yolo' });
+
+	return 'Ready!'
+}
