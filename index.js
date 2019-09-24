a614324ddf1cd13e667f6ba538807332317eeeb1
chris <parweb@gmail.com>
2019-09-24 11:51:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,22 @@ const winston = require('winston');
 const MongoDB = require('winston-mongodb').MongoDB;
 
 const options = {
-    db : 'mongodb://host:port/dbname',
-    username : 'user',
-    password : 'password',
-    collection : 'logs',
-    level : 'info',
-    storeHost: true
+  db: 'mongodb://host:port/dbname',
+  username: 'user',
+  password: 'password',
+  collection: 'logs',
+  level: 'info',
+  storeHost: true
 };
 
-winston.loggers.add('mongoLog',{
-    transports : [
-        new(winston.transports.MongoDB)(options),
-    ]
+winston.loggers.add('mongoLog', {
+  transports: [new winston.transports.MongoDB(options)]
 });
 
-var logger = winston.loggers.get('mongoLog')
+var logger = winston.loggers.get('mongoLog');
 
 module.exports = async (req, res) => {
-	logger.info({'test message': 'plop'}, { 'plop' : 'yolo' });
+  logger.info({ 'test message': 'plop' }, { plop: 'yolo' });
 
-	return 'Ready!'
-}
+  return 'Ready!';
+};
